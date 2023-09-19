97d5d3091d083cdaeb23602ec54e18c7a8b4a836
Anaks-CT <anaksthayyil30@gmail.com>
2023-09-19 22:58:42 +0530

    wrapped the home page with the provider which gives the session for authentication

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+"use client";
 
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
-}
+import { SessionProvider } from "next-auth/react";
 
-export default Provider
+const Provider = ({ session, children }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;
