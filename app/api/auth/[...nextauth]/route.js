471bc9aff454f544a5611a5d1818927f2a47cace
Anaks-CT <anaksthayyil30@gmail.com>
2023-09-20 10:06:32 +0530

    connected database

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,25 @@
+import { connectToDB } from "@utils/database"
+import NextAuth from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    async session({session}){
+
+    },
+    async signIn({profile}){
+        try {
+            await connectToDB()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+})
+
+export {handler as GET, handler as POST}
---
+++ utils/database.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true); // to avoid the error in the console
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  // connecting the mongodb if it is not connected
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
